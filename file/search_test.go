774a4527fcebb06a98ac9b6dff3594ca6d5827b6
Spiegel <spiegel.im.spiegel@gmail.com>
2018-03-07 22:46:23 +0900

    Update test for file subpackage

--- file/search_test.go
+++ file/search_test.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -11,13 +12,14 @@ func TestUiSearch(t *testing.T) {
 		result []string
 	}{
 		{path: "XXX/**/", result: []string{}},
-		{path: "**/", result: []string{"./", "testdata/"}},
-		{path: "**/*", result: []string{"search.go", "search_test.go", "testdata/", "testdata/source.c", "testdata/source.h"}},
-		{path: "./**/*", result: []string{"search.go", "search_test.go", "testdata/", "testdata/source.c", "testdata/source.h"}},
-		{path: "testdata/**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "./**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "**/../**/*.[ch]", result: []string{"../file/testdata/source.c", "../file/testdata/source.h", "testdata/source.c", "testdata/source.h"}},
+		{path: "**/", result: []string{"./", "testdata/", "testdata/include/"}},
+		{path: "**/*", result: []string{"search.go", "search_test.go", "testdata/", "testdata/include/", "testdata/include/source.h", "testdata/source.c"}},
+		{path: "./*/*", result: []string{"testdata/include/", "testdata/source.c"}},
+		{path: "./**/*", result: []string{"search.go", "search_test.go", "testdata/", "testdata/include/", "testdata/include/source.h", "testdata/source.c"}},
+		{path: "testdata/**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "./**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "**/../**/*.[ch]", result: []string{"../file/testdata/include/source.h", "../file/testdata/source.c", "testdata/include/source.h", "testdata/source.c"}},
 	}
 	for _, tc := range testCases {
 		str := Search(tc.path, NewSearchOption(
@@ -36,11 +38,12 @@ func TestUiSearchFileOnly(t *testing.T) {
 	}{
 		{path: "XXX/**/", result: []string{}},
 		{path: "**/", result: []string{}},
-		{path: "./**/*", result: []string{"search.go", "search_test.go", "testdata/source.c", "testdata/source.h"}},
-		{path: "testdata/**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "./**/*.[ch]", result: []string{"testdata/source.c", "testdata/source.h"}},
-		{path: "**/../**/*.[ch]", result: []string{"../file/testdata/source.c", "../file/testdata/source.h", "testdata/source.c", "testdata/source.h"}},
+		{path: "./*/*", result: []string{"testdata/source.c"}},
+		{path: "./**/*", result: []string{"search.go", "search_test.go", "testdata/include/source.h", "testdata/source.c"}},
+		{path: "testdata/**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "./**/*.[ch]", result: []string{"testdata/include/source.h", "testdata/source.c"}},
+		{path: "**/../**/*.[ch]", result: []string{"../file/testdata/include/source.h", "../file/testdata/source.c", "testdata/include/source.h", "testdata/source.c"}},
 	}
 	for _, tc := range testCases {
 		str := Search(tc.path, NewSearchOption(
@@ -60,13 +63,14 @@ func TestUiSearchDirOnly(t *testing.T) {
 		result []string
 	}{
 		{path: "XXX/**/", result: []string{}},
-		{path: "**/", result: []string{"./", "testdata/"}},
-		{path: "**/*", result: []string{"testdata/"}},
-		{path: "./**/*", result: []string{"testdata/"}},
+		{path: "**/", result: []string{"./", "testdata/", "testdata/include/"}},
+		{path: "*", result: []string{"testdata/"}},
+		{path: "**/*", result: []string{"testdata/", "testdata/include/"}},
+		{path: "./**/*", result: []string{"testdata/", "testdata/include/"}},
 		{path: "testdata", result: []string{"testdata/"}},
-		{path: "testdata/**/", result: []string{"testdata/"}},
+		{path: "testdata/**/", result: []string{"testdata/", "testdata/include/"}},
 		{path: "test*", result: []string{"testdata/"}},
-		{path: "test*/**/", result: []string{"testdata/"}},
+		{path: "test*/**/", result: []string{"testdata/", "testdata/include/"}},
 		{path: "test*/**/XXX", result: []string{}},
 	}
 	for _, tc := range testCases {
@@ -87,13 +91,13 @@ func TestUiSearchAbs(t *testing.T) {
 		result []string
 	}{
 		{path: "XXX/**/", result: []string{}},
-		{path: "**/", result: []string{"/path/to/file/", "/path/to/file/testdata/"}},
-		{path: "**/*", result: []string{"/path/to/file/search.go", "/path/to/file/search_test.go", "/path/to/file/testdata/", "/path/to/file/testdata/source.c", "testdata/source.h"}},
-		{path: "./**/*", result: []string{"/path/to/file/search.go", "/path/to/file/search_test.go", "/path/to/file/testdata/", "/path/to/file/testdata/source.c", "/path/to/file/testdata/source.h"}},
+		{path: "**/", result: []string{"/path/to/file/", "/path/to/file/testdata/", "/path/to/file/testdata/include"}},
+		{path: "**/*", result: []string{"/path/to/file/search.go", "/path/to/file/search_test.go", "/path/to/file/testdata/", "/path/to/file/testdata/include", "/path/to/file/testdata/include/source.h", "/path/to/file/testdata/source.c"}},
+		{path: "./**/*", result: []string{"/path/to/file/search.go", "/path/to/file/search_test.go", "/path/to/file/testdata/", "/path/to/file/testdata/include", "/path/to/file/testdata/include/source.h", "/path/to/file/testdata/source.c"}},
 		{path: "testdata/**/*.[ch]", result: []string{"/path/to/file/testdata/source.c", "/path/to/file/testdata/source.h"}},
-		{path: "**/*.[ch]", result: []string{"/path/to/file/testdata/source.c", "/path/to/file/testdata/source.h"}},
-		{path: "./**/*.[ch]", result: []string{"/path/to/file/testdata/source.c", "/path/to/file/testdata/source.h"}},
-		{path: "**/../**/*.[ch]", result: []string{"/path/to/file/testdata/source.c", "/path/to/file/testdata/source.h"}},
+		{path: "**/*.[ch]", result: []string{"/path/to/file/testdata/include/source.h", "/path/to/file/testdata/source.c"}},
+		{path: "./**/*.[ch]", result: []string{"/path/to/file/testdata/include/source.h", "/path/to/file/testdata/source.c"}},
+		{path: "**/../**/*.[ch]", result: []string{"/path/to/file/testdata/include/source.h", "/path/to/file/testdata/source.c"}},
 	}
 	for _, tc := range testCases {
 		str := Search(tc.path, NewSearchOption(
@@ -104,4 +108,11 @@ func TestUiSearchAbs(t *testing.T) {
 			t.Errorf("Search(\"%s\")  = %v, want %v.", tc.path, str, tc.result)
 		}
 	}
+}
+
+func ExampleSearch() {
+	result := Search("**/*.[ch]", NewSearchOption(WithToSlash(true)))
+	fmt.Println(result)
+	// Output:
+	// [testdata/include/source.h testdata/source.c]
 }
